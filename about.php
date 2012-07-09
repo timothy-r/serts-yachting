9f2f094a9c363dc11205256f70e481452caa2f3b
trodger <tim.rodger@gmail.com>
2012-07-09 22:16:18 +0100

    first commit

---
+++ about.php
@@ -0,0 +1,131 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>About sailing holidays in Turkey</title>
+
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.js"></script>
+<script type="text/javascript">
+
+function slideSwitch() {
+    var $active = $('#slideshow a.active');
+
+    if ( $active.length == 0 ) $active = $('#slideshow a:last');
+
+        var $next =  $active.next().length ? $active.next()
+        : $('#slideshow a:first');
+
+    $active.addClass('last-active');
+
+    $next.css({opacity: 0.0})
+        .addClass('active')
+        .animate({opacity: 1.0}, 1000, function() {
+            $active.removeClass('active last-active');
+        });
+}
+
+$(function() {
+    setInterval( "slideSwitch()", 5000 );
+});
+
+</script>
+
+<link href="style.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+<?php include("header.php"); ?>
+
+<div style="clear:both;"></div>
+<div id="content">
+
+<div class="infobox">
+<table width="100%" border="0" cellspacing="2" cellpadding="2">
+  <tr>
+    <td><h1>About Serts Yachting</h1></td>
+    <td colspan="2">&nbsp;</td>
+  </tr>
+  <tr>
+    <td width="68%" align="left" valign="top"><div>
+      <p><span id="internal-source-marker_0.6878857291303575">We are an English / Turkish family run business. Our Gulet &lsquo;His&rsquo; (pronounced 'hiss', which in Turkish means good feeling), is a traditional, wooden, two masted sailing yacht. She is 18 metres in length, fully air conditioned with six en-suite double cabins.</span></p>
+      <p><span> There is a large decking area perfect for soaking up the Mediterranean sun and a  spacious shaded seating area in which to enjoy cocktails, nibbles and that all important afternoon siesta.</span></p>
+    </div></td>
+    <td width="16%"><img src="old/karen.png" width="150" height="98"></td>
+    <td width="16%"><img src="old/mainsale.png" width="150" height="98"></td>
+  </tr>
+  <tr>
+    <td align="left" valign="top"><h2>&nbsp;</h2></td>
+    <td colspan="2">&nbsp;</td>
+  </tr>
+  <tr>
+    <td align="left" valign="top"><div>
+      <div>
+        <h2>          Boat Information        </h2>
+        <div dir="ltr">
+          <table width="100%" cellpadding="5" cellspacing="5">
+            <colgroup>
+              <col width="169">
+              <col width="80">
+              </colgroup>
+            <tbody>
+              <tr>
+                <td><strong>LENGTH</strong></td>
+                <td><strong>18 Meters</strong></td>
+              </tr>
+              <tr>
+                <td><strong>MASTS</strong></td>
+                <td><strong>2</strong></td>
+              </tr>
+              <tr>
+                <td><strong>ENGINE</strong></td>
+                <td><strong>165hp Ford</strong></td>
+              </tr>
+              <tr>
+                <td><strong>AIR CON</strong></td>
+                <td><strong>Full air con</strong></td>
+              </tr>
+              <tr>
+                <td><strong>GENERATOR</strong></td>
+                <td><strong>220volts</strong></td>
+              </tr>
+              <tr>
+                <td><strong>FRESH WATER TANK CAPACITY</strong></td>
+                <td><strong>2 tonnes </strong></td>
+              </tr>
+              <tr>
+                <td><strong>WASTE WATER TANK CAPACITY</strong></td>
+                <td><strong>272 gallons </strong></td>
+              </tr>
+            </tbody>
+          </table>
+    </div>
+      </div>
+<p><br>
+      </p>
+    </div></td>
+    <td align="left" valign="top"><img src="old/burn-karen.png" width="150" height="98"><br>
+      <br></td>
+    <td align="center" valign="top"><img src="old/oz.png" width="150" height="98"><br>
+      <br>      <a href="http://www.facebook.com/pages/Serts-Yachting/158652700888125" target="_blank"><img src="grx/facebook.jpg" width="133" height="40" border="0"></a></td>
+  </tr>
+</table>
+<div style="clear:both;"></div>
+</div>
+</div>
+
+
+
+
+
+
+
+
+<div style="clear:both;"></div>
+<div id="info" style="background-image:url(grx/top-bkg.jpg); background-repeat:no-repeat;">
+  <p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+</div>
+</body>
+</html>
---
+++ booking.php
@@ -0,0 +1,163 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>Sailing Holiday Availability | Serts Yachting</title>
+
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.js"></script>
+<script type="text/javascript">
+
+function slideSwitch() {
+    var $active = $('#slideshow a.active');
+
+    if ( $active.length == 0 ) $active = $('#slideshow a:last');
+
+        var $next =  $active.next().length ? $active.next()
+        : $('#slideshow a:first');
+
+    $active.addClass('last-active');
+
+    $next.css({opacity: 0.0})
+        .addClass('active')
+        .animate({opacity: 1.0}, 1000, function() {
+            $active.removeClass('active last-active');
+        });
+}
+
+$(function() {
+    setInterval( "slideSwitch()", 5000 );
+});
+
+</script>
+
+<link href="style.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+<?php include("header.php"); ?>
+<div style="clear:both;"></div>
+<div id="content">
+  <div class="infobox">
+  <table width="100%" border="0" cellspacing="2" cellpadding="2">
+  <tr>
+    <td width="68%" height="39"><h1>Mediterranian Cruise availability</h1></td>
+    <td width="32%">&nbsp;</td>
+  </tr>
+  <tr>
+    <td colspan="2" align="left" valign="top"><iframe src="https://www.google.com/calendar/embed?showTitle=0&amp;showPrint=0&amp;showTabs=0&amp;showCalendars=0&amp;showTz=0&amp;height=500&amp;wkst=1&amp;bgcolor=%23f9f9f9&amp;src=eprcoocrb55o73fk05enq51o5k%40group.calendar.google.com&amp;color=%23125A12&amp;ctz=Europe%2FIstanbul" style=" border-width:0 " width="920" height="500" frameborder="0" scrolling="no"></iframe></td>
+    
+    
+
+    </td>
+    </tr>
+  <tr>
+    <td colspan="2" align="left" valign="top">&nbsp;</td>
+    <td>  
+  </tr>
+  <tr>
+    <td colspan="2" align="left" valign="top"><p><strong>Destination gocek and fethiye bay </strong></p>
+      <p>To book your cruise holiday you need to provide a deposit of a third of the total value. To make this easier we have provided payment buttons online. If you wish to pay by cheque or another means please contact us for information.</p>
+      <table width="100%" border="0" cellspacing="5" cellpadding="5">
+        <tr>
+          <td width="53%"> <p> <strong>Mid season; beginning of may, end of june</strong>
+              <br>
+              £2400 (&euro;2800) per week (deposit £800)<br>
+              daily price £375  (&euro;440)</p></td>
+          <td width="47%" align="right"><form action="https://www.paypal.com/cgi-bin/webscr" method="post">
+<input type="hidden" name="cmd" value="_s-xclick">
+<input type="hidden" name="hosted_button_id" value="UACAP79FB24DW">
+<table>
+<tr><td><input type="hidden" name="on0" value="Please Specify the Dates">Please Specify the Dates</td>
+  <td rowspan="2"><input type="image" src="https://www.paypalobjects.com/en_GB/i/btn/btn_paynowCC_LG.gif" border="0" name="submit2" alt="PayPal — The safer, easier way to pay online."></td>
+</tr>
+<tr><td><input type="text" name="os0" maxlength="200"></td>
+  </tr>
+</table>
+<img alt="" border="0" src="https://www.paypalobjects.com/en_GB/i/scr/pixel.gif" width="1" height="1">
+          </form>
+</td>
+        </tr>
+      </table>
+      <table width="100%" border="0" cellspacing="5" cellpadding="5">
+        <tr>
+          <td width="53%"><strong>High season; beginning of July- August September 15th </strong><br>
+            weekly price; £2900  (&euro;3380) per week (deposit £950)<br>
+Daily price £450  (&euro;525))</td>
+          <td width="47%" align="right"><form action="https://www.paypal.com/cgi-bin/webscr" method="post">
+<input type="hidden" name="cmd" value="_s-xclick">
+<input type="hidden" name="hosted_button_id" value="HAK2TVQNA5NYE">
+<table>
+<tr><td><input type="hidden" name="on0" value="Please Specify the Dates">Please Specify the Dates</td>
+  <td rowspan="2"><input type="image" src="https://www.paypalobjects.com/en_GB/i/btn/btn_paynowCC_LG.gif" border="0" name="submit3" alt="PayPal — The safer, easier way to pay online."></td>
+</tr>
+<tr><td><input type="text" name="os0" maxlength="200"></td>
+  </tr>
+</table>
+<img alt="" border="0" src="https://www.paypalobjects.com/en_GB/i/scr/pixel.gif" width="1" height="1">
+          </form>
+</td>
+        </tr>
+      </table>
+      <table width="100%" border="0" cellspacing="5" cellpadding="5">
+        <tr>
+          <td width="53%"><strong>Mid season; September 15th,-October 15th</strong><br>
+            Weekly price; £2400  (&euro;2800) per week (deposit £800)<br>
+            Daily price £375  (&euro;440)</td>
+          <td width="47%" align="right"><form action="https://www.paypal.com/cgi-bin/webscr" method="post">
+<input type="hidden" name="cmd" value="_s-xclick">
+<input type="hidden" name="hosted_button_id" value="UACAP79FB24DW">
+<table>
+<tr><td><input type="hidden" name="on0" value="Please Specify the Dates">Please Specify the Dates</td>
+  <td rowspan="2"><input type="image" src="https://www.paypalobjects.com/en_GB/i/btn/btn_paynowCC_LG.gif" border="0" name="submit4" alt="PayPal — The safer, easier way to pay online."></td>
+</tr>
+<tr><td><input type="text" name="os0" maxlength="200"></td>
+  </tr>
+</table>
+<img alt="" border="0" src="https://www.paypalobjects.com/en_GB/i/scr/pixel.gif" width="1" height="1">
+          </form>
+</td>
+        </tr>
+      </table>
+      <table width="100%" border="0" cellspacing="5" cellpadding="5">
+        <tr>
+          <td width="53%"><strong>Low season October 15th-November 15th</strong><br>
+Weekly Price £2000  (&euro;2330) per week (deposit £650)<br>
+Daily price £325 (&euro;380)</td>
+          <td width="47%" align="right"><form action="https://www.paypal.com/cgi-bin/webscr" method="post">
+<input type="hidden" name="cmd" value="_s-xclick">
+<input type="hidden" name="hosted_button_id" value="WLNS6FX2CGBWG">
+<table>
+<tr><td><input type="hidden" name="on0" value="Please Specify the Dates">Please Specify the Dates</td>
+  <td rowspan="2"><input type="image" src="https://www.paypalobjects.com/en_GB/i/btn/btn_paynowCC_LG.gif" border="0" name="submit" alt="PayPal — The safer, easier way to pay online."></td>
+</tr>
+<tr><td><input type="text" name="os0" maxlength="200"></td>
+  </tr>
+</table>
+<img alt="" border="0" src="https://www.paypalobjects.com/en_GB/i/scr/pixel.gif" width="1" height="1">
+          </form>
+</td>
+        </tr>
+      </table>
+      <p> Other routes such as Marmaris,Bodrum,Kas,Antalya etc.will be priced according to distance.</p></td>
+    <td>  
+  </tr>
+  </table>
+</div>
+</div>
+
+
+
+
+
+
+
+
+<div style="clear:both;"></div>
+<div id="info" style="background-image:url(grx/top-bkg.jpg); background-repeat:no-repeat;">
+  <p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+</div>
+</body>
+</html>
