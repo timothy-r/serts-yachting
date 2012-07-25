8946e2d9ce71fd2052e14e1c20bf5a3a3a00a0db
trodger <tim.rodger@gmail.com>
2012-07-25 20:51:39 +0100

    removed booking forms

--- booking.php
+++ booking.php
@@ -30,7 +30,7 @@ $(function() {
 
 </script>
 
-<link href="css/style.css" rel="stylesheet" type="text/css">
+<link href="css/style.css" rel="stylesheet" type="text/css" />
 </head>
 
 <body>
@@ -46,36 +46,21 @@ $(function() {
   <tr>
     <td colspan="2" align="left" valign="top"><iframe src="https://www.google.com/calendar/embed?showTitle=0&amp;showPrint=0&amp;showTabs=0&amp;showCalendars=0&amp;showTz=0&amp;height=500&amp;wkst=1&amp;bgcolor=%23f9f9f9&amp;src=eprcoocrb55o73fk05enq51o5k%40group.calendar.google.com&amp;color=%23125A12&amp;ctz=Europe%2FIstanbul" style=" border-width:0 " width="920" height="500" frameborder="0" scrolling="no"></iframe></td>
     
-    
-
     </td>
     </tr>
   <tr>
     <td colspan="2" align="left" valign="top">&nbsp;</td>
-    <td>  
+    <td></td>
   </tr>
   <tr>
     <td colspan="2" align="left" valign="top"><p><strong>Destination gocek and fethiye bay </strong></p>
-      <p>To book your cruise holiday you need to provide a deposit of a third of the total value. To make this easier we have provided payment buttons online. If you wish to pay by cheque or another means please contact us for information.</p>
+      <p>To book your cruise holiday you need to provide a deposit of a third of the total value. Please contact us for information on how to pay.</p>
       <table width="100%" border="0" cellspacing="5" cellpadding="5">
         <tr>
           <td width="53%"> <p> <strong>Mid season; beginning of may, end of june</strong>
               <br>
               £2400 (&euro;2800) per week (deposit £800)<br>
               daily price £375  (&euro;440)</p></td>
-          <td width="47%" align="right"><form action="https://www.paypal.com/cgi-bin/webscr" method="post">
-<input type="hidden" name="cmd" value="_s-xclick">
-<input type="hidden" name="hosted_button_id" value="UACAP79FB24DW">
-<table>
-<tr><td><input type="hidden" name="on0" value="Please Specify the Dates">Please Specify the Dates</td>
-  <td rowspan="2"><input type="image" src="https://www.paypalobjects.com/en_GB/i/btn/btn_paynowCC_LG.gif" border="0" name="submit2" alt="PayPal — The safer, easier way to pay online."></td>
-</tr>
-<tr><td><input type="text" name="os0" maxlength="200"></td>
-  </tr>
-</table>
-<img alt="" border="0" src="https://www.paypalobjects.com/en_GB/i/scr/pixel.gif" width="1" height="1">
-          </form>
-</td>
         </tr>
       </table>
       <table width="100%" border="0" cellspacing="5" cellpadding="5">
@@ -83,19 +68,6 @@ $(function() {
           <td width="53%"><strong>High season; beginning of July- August September 15th </strong><br>
             weekly price; £2900  (&euro;3380) per week (deposit £950)<br>
 Daily price £450  (&euro;525))</td>
-          <td width="47%" align="right"><form action="https://www.paypal.com/cgi-bin/webscr" method="post">
-<input type="hidden" name="cmd" value="_s-xclick">
-<input type="hidden" name="hosted_button_id" value="HAK2TVQNA5NYE">
-<table>
-<tr><td><input type="hidden" name="on0" value="Please Specify the Dates">Please Specify the Dates</td>
-  <td rowspan="2"><input type="image" src="https://www.paypalobjects.com/en_GB/i/btn/btn_paynowCC_LG.gif" border="0" name="submit3" alt="PayPal — The safer, easier way to pay online."></td>
-</tr>
-<tr><td><input type="text" name="os0" maxlength="200"></td>
-  </tr>
-</table>
-<img alt="" border="0" src="https://www.paypalobjects.com/en_GB/i/scr/pixel.gif" width="1" height="1">
-          </form>
-</td>
         </tr>
       </table>
       <table width="100%" border="0" cellspacing="5" cellpadding="5">
@@ -103,19 +75,6 @@ Daily price £450  (&euro;525))</td>
           <td width="53%"><strong>Mid season; September 15th,-October 15th</strong><br>
             Weekly price; £2400  (&euro;2800) per week (deposit £800)<br>
             Daily price £375  (&euro;440)</td>
-          <td width="47%" align="right"><form action="https://www.paypal.com/cgi-bin/webscr" method="post">
-<input type="hidden" name="cmd" value="_s-xclick">
-<input type="hidden" name="hosted_button_id" value="UACAP79FB24DW">
-<table>
-<tr><td><input type="hidden" name="on0" value="Please Specify the Dates">Please Specify the Dates</td>
-  <td rowspan="2"><input type="image" src="https://www.paypalobjects.com/en_GB/i/btn/btn_paynowCC_LG.gif" border="0" name="submit4" alt="PayPal — The safer, easier way to pay online."></td>
-</tr>
-<tr><td><input type="text" name="os0" maxlength="200"></td>
-  </tr>
-</table>
-<img alt="" border="0" src="https://www.paypalobjects.com/en_GB/i/scr/pixel.gif" width="1" height="1">
-          </form>
-</td>
         </tr>
       </table>
       <table width="100%" border="0" cellspacing="5" cellpadding="5">
@@ -123,19 +82,6 @@ Daily price £450  (&euro;525))</td>
           <td width="53%"><strong>Low season October 15th-November 15th</strong><br>
 Weekly Price £2000  (&euro;2330) per week (deposit £650)<br>
 Daily price £325 (&euro;380)</td>
-          <td width="47%" align="right"><form action="https://www.paypal.com/cgi-bin/webscr" method="post">
-<input type="hidden" name="cmd" value="_s-xclick">
-<input type="hidden" name="hosted_button_id" value="WLNS6FX2CGBWG">
-<table>
-<tr><td><input type="hidden" name="on0" value="Please Specify the Dates">Please Specify the Dates</td>
-  <td rowspan="2"><input type="image" src="https://www.paypalobjects.com/en_GB/i/btn/btn_paynowCC_LG.gif" border="0" name="submit" alt="PayPal — The safer, easier way to pay online."></td>
-</tr>
-<tr><td><input type="text" name="os0" maxlength="200"></td>
-  </tr>
-</table>
-<img alt="" border="0" src="https://www.paypalobjects.com/en_GB/i/scr/pixel.gif" width="1" height="1">
-          </form>
-</td>
         </tr>
       </table>
       <p> Other routes such as Marmaris,Bodrum,Kas,Antalya etc.will be priced according to distance.</p></td>
@@ -144,13 +90,6 @@ Daily price £325 (&euro;380)</td>
   </table>
 </div>
 </div>
-
-
-
-
-
-
-
 
 <div style="clear:both;"></div>
 <div id="info" style="background-image:url(img/top-bkg.jpg); background-repeat:no-repeat;">
